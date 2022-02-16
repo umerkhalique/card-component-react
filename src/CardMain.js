6a788d82f6be1c5e12e41350bbd7d874bd261b00
umer khalique <omerkhalique13@gmail.com>
2022-02-16 16:44:43 +0500

    fix

--- src/CardMain.js
+++ src/CardMain.js
@@ -2,15 +2,37 @@ import React from 'react';
 import "./style.css";
 import eth from './images/image-equilibrium.jpg';
 
+
+
+
 const CardMain = () => {
     return(
         <div className="main-div">
             <div className="card">
                 <div className="img">
-                <img src={eth} />
+                    <div className="img-div-root"><img src={eth} /></div>
                 </div>
                 <div className="content">
-                    hithere again
+                    <div>
+                        <h1>Equilibrium #3429</h1>
+                    </div>
+                    <div>
+                        <p>Our Equilibrium collection promotes balance and calm.</p>
+                    </div>
+                    <div className="content2">
+                        <div>
+                        
+                        </div>
+                        <div>
+                            <p>ddd</p>
+                        </div>
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
                 </div>
             </div>
         </div>
